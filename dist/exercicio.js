576d144287c49ffcb4a52fff54d5783cfe3afcb9
Omegahub <Omegahub2019@gmail.com>
2019-08-07 21:58:11 -0300

    exercicio aprovado ou reprovado

---
+++ dist/exercicio.js
@@ -0,0 +1,11 @@
+var nota1 = 10;
+var nota2 = 9;
+var nota3 = 5;
+var media = (nota1 + nota2 + nota3) / 3;
+console.log("A media é : " + media);
+if (media >= 7) {
+    console.log("aprovado " + media);
+}
+else {
+    console.log("reprovado " + media);
+}

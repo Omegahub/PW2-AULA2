83939333ef19add5530581ea71ea90d9a138da81
Omegahub <Omegahub2019@gmail.com>
2019-08-07 21:46:45 -0300

    inclusao do noEmitOnError

---
+++ dist/exemplo2.js
@@ -0,0 +1,5 @@
+var qtde = 5;
+qtde = "5";
+console.log("quantidade é " + qtde);
+qtde = qtde + 1;
+console.log("novo valor de " + qtde + " qtde");
---
+++ exemplo2.ts
@@ -0,0 +1,5 @@
+let qtde : number = 5
+qtde= "5"
+console.log("quantidade é " + qtde)
+qtde = qtde + 1
+console.log("novo valor de " + qtde +" qtde")
